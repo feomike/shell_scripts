881f6c264d6c0640bd4cc87f4fa2abe5743be879
Michael Byrne <michael.byrne@fcc.gov>
2014-03-30 07:55:21 -0400

    Create census_2009_download.sh

---
+++ census_2009_download.sh
@@ -0,0 +1,62 @@
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/01_ALABAMA/tl_2009_01_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/02_ALASKA/tl_2009_02_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/04_ARIZONA/tl_2009_04_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/05_ARKANSAS/tl_2009_05_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/06_CALIFORNIA/tl_2009_06_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/08_COLORADO/tl_2009_08_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/09_CONNECTICUT/tl_2009_09_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/10_DELAWARE/tl_2009_10_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/11_DISTRICT_OF_COLUMBIA/tl_2009_11_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/12_FLORIDA/tl_2009_12_tabblock.zip
+
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/13_GEORGIA/tl_2009_13_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/15_HAWAII/tl_2009_15_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/16_IDAHO/tl_2009_16_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/17_ILLINOIS/tl_2009_17_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/18_INDIANA/tl_2009_18_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/19_IOWA/tl_2009_19_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/20_KANSAS/tl_2009_20_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/21_KENTUCKY/tl_2009_21_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/22_LOUISIANA/tl_2009_22_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/23_MAINE/tl_2009_23_tabblock.zip
+
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/24_MARYLAND/tl_2009_24_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/25_MASSACHUSETTS/tl_2009_25_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/26_MICHIGAN/tl_2009_26_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/27_MINNESOTA/tl_2009_27_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/28_MISSISSIPPI/tl_2009_28_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/29_MISSOURI/tl_2009_29_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/30_MONTANA/tl_2009_30_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/31_NEBRASKA/tl_2009_31_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/32_NEVADA/tl_2009_32_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/33_NEW_HAMPSHIRE/tl_2009_33_tabblock.zip
+
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/34_NEW_JERSEY/tl_2009_34_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/35_NEW_MEXICO/tl_2009_35_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/36_NEW_YORK/tl_2009_36_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/37_NORTH_CAROLINA/tl_2009_37_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/38_NORTH_DAKOTA/tl_2009_38_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/39_OHIO/tl_2009_39_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/40_OKLAHOMA/tl_2009_40_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/41_OREGON/tl_2009_41_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/42_PENNSYLVANIA/tl_2009_42_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/44_RHODE_ISLAND/tl_2009_44_tabblock.zip
+
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/45_SOUTH_CAROLINA/tl_2009_45_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/46_SOUTH_DAKOTA/tl_2009_46_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/47_TENNESSEE/tl_2009_47_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/48_TEXAS/tl_2009_48_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/49_UTAH/tl_2009_49_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/50_VERMONT/tl_2009_50_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/51_VIRGINIA/tl_2009_51_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/53_WASHINGTON/tl_2009_53_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/54_WEST_VIRGINIA/tl_2009_54_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/55_WISCONSIN/tl_2009_55_tabblock.zip
+
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/56_WYOMING/tl_2009_56_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/60_AMERICAN_SAMOA/tl_2009_60_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/66_GUAM/tl_2009_66_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/69_COMMONWEALTH_OF_THE_NORTHERN_MARIANA_ISLANDS/tl_2009_69_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/72_PUERTO_RICO/tl_2009_72_tabblock.zip
+ftp ftp://ftp2.census.gov/geo/tiger/TIGER2009/78_VIRGIN_ISLANDS_OF_THE_UNITED_STATES/tl_2009_78_tabblock.zip
+unzip *_tabblock.zip -d ./
